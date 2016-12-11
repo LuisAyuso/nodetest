b204ae9c0990b09b4298c1b2fd79daee7ebe5202
Luis Ayuso <luis.f.ayuso@gmail.com>
2016-12-11 12:20:32 +0100

    init, with submodules istead of extern

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,4 +1,9 @@
+#include <QtWidgets/QApplication>
 
+#include <nodes/NodeData>
+#include <nodes/FlowScene>
+#include <nodes/FlowView>
+#include <nodes/DataModelRegistry>
 
 int main(){
     return 0;
