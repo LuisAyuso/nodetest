a998fd6edbf0581decec63ef1edb45502a3323cc
Luis Ayuso <luis.f.ayuso@gmail.com>
2016-12-11 13:58:16 +0100

    ycm enable

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -5,6 +5,15 @@
 #include <nodes/FlowView>
 #include <nodes/DataModelRegistry>
 
-int main(){
-    return 0;
+int main(int argc, char **argv){
+    QApplication app(argc, argv);
+  
+    FlowScene scene(registerDataModels());
+    FlowView view(&scene);
+  
+    view.setWindowTitle("Node-based flow editor");
+    view.resize(800, 600);
+    view.show();
+
+	return app.exec();
 }
